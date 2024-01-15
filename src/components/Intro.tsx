4752bdbb1018a1284387caf500816f90a710948b
dongmin115 <dongmin11566@gmail.com>
2024-01-16 03:28:14 +0900

    style : 1페이지

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -4,11 +4,6 @@ const Intro = () => {
   return (
     <div>
       <div className="slide-container relative group w-130 h-full overflow-hidden bg-black m-auto mt-46">
-        <img
-          className="object-cover w-full h-full transform duration-700 backdrop-opacity-100"
-          src="https://i.ibb.co/wKt0TKS/m-5.gif"
-          alt=""
-        />
       </div>
     </div>
   );
--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,14 +2,20 @@ import React from "react";
 
 const Title = () => {
   return (
-    <div>
-      <div className="slide-container relative group w-130 h-full overflow-hidden bg-black m-auto mt-46">
-        <img
-          className="object-cover w-full h-full transform duration-700 backdrop-opacity-100"
-          src="https://uk.fi-group.com/wp-content/uploads/sites/7/2022/04/FI-Connect-1920x700.jpg"
-          alt=""
-        />
+    <div className="">
+    <div className="bg-[#90adcd] w-screen h-screen flex flex-row items-center justify-evenly">
+      <div className="flex flex-col items-start">
+      <span className=" text-[#0d3a5c] font-normal sm:text-4xl md:text-8xl">ClipTab</span>
+      <span className="text-[#0d3a5c] self-end sm:text-xl md:text-lg">World Management</span>
       </div>
+      <img 
+      className="w-2/5 h-2/5"
+      src="https://mteam.my.canva.site/onboarding-web/images/3dbc505e2279eade54f64358ae99f75b.svg" 
+      alt="이미지1"/>
+    </div>
+    <div className="bg-[#90adcd] w-screen h-screen flex flex-row items-center justify-evenly">
+      
+    </div>
     </div>
   );
 };
